c6f4f12e3c786690ddf09a33580924fa32341b74
zhuchuandong86 <zhuchuandong@139.com>
2025-12-22 10:19:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ SMTP_PORT = 587
 SENDER_EMAIL = "zhuchuandong@gmail.com"        # 发件账号
 SENDER_PASSWORD = "eupksymngvybzuac"        # 应用专用密码
 # 接收人列表 (用英文逗号隔开)
-RECEIVER_EMAIL = "zhuchuandong@huawei.com"
+RECEIVER_EMAIL = "zhuchuandong@huawei.com,yt.tangyong@huawei.com"
 
 # ================= 系统配置区 (不要动) =================
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -22,4 +22,5 @@ RAW_NEWS_FILE = os.path.join(DATA_DIR, "raw_news.json")
 REPORT_FILE = os.path.join(DATA_DIR, "report.html")
 
 if not os.path.exists(DATA_DIR):
+
     os.makedirs(DATA_DIR)
